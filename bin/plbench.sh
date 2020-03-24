25bb0c5cdc161f12e3c0bf6bf38e55216a98e05e
Jose Zalacain <Jose Zalacain@DESKTOP-NK7F6C3>
2020-03-24 17:50:54 -0400

    Initial commit

---
+++ bin/plbench.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
+if [ -e "/usr/bin/cygpath" ]
+then
+  LIB_PATH="$(cygpath -w $SCRIPT_PATH/lib/)*"
+else
+  LIB_PATH="$SCRIPT_PATH/lib/*"
+fi
+$SCRIPT_PATH/jre/bin/java -classpath "$LIB_PATH" org.prolobjectlink.prolog.benchmark.BenchmarkRunner ${1+"$@"}
