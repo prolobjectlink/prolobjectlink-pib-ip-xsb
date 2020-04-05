f30b51eafb9368d2fadb81396c067b12e51f8e22
Jose Zalacain <Jose Zalacain@DESKTOP-NK7F6C3>
2020-04-05 11:15:51 -0400

    Introduce dereference benchmark

--- bin/plbench.sh
+++ bin/plbench.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/bash
-SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
-if [ -e "/usr/bin/cygpath" ]
-then
-  LIB_PATH="$(cygpath -w $SCRIPT_PATH/lib/)*"
-else
-  LIB_PATH="$SCRIPT_PATH/lib/*"
-fi
-$SCRIPT_PATH/jre/bin/java -classpath "$LIB_PATH" io.github.prolobjectlink.prolog.benchmark.BenchmarkRunner ${1+"$@"}
+java -classpath "$(dirname "$(pwd)")/lib/*" io.github.prolobjectlink.prolog.ip.xsb.XsbBenchmarkRunner ${1+"$@"}
